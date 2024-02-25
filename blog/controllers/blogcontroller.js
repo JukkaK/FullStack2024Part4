8caca5a024c101ace167139b65d5467b9b98ca7b
JukkaK <jukka.koskelin@zure.com>
2024-02-25 09:46:50 +0200

    4.19

--- blog/controllers/blogcontroller.js
+++ blog/controllers/blogcontroller.js
@@ -2,6 +2,15 @@ const blogRouter = require('express').Router()
 const Blog = require('../models/blog')
 const logger = require('../utils/logger')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '')
+  }
+  return null
+}
 
 blogRouter.get('/', async (request, response) => {
     logger.info('hitting get all in router')    
@@ -25,8 +34,11 @@ blogRouter.get('/:id', async (request, response, next) => {
 blogRouter.post('/', async (request, response, next) => {    
   const body = request.body
   
-  const user = await User.findOne({})
-  //const user = await User.findById(body.userId)
+  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token invalid' })
+  }
+  const user = await User.findById(decodedToken.id)
 
   const blog = new Blog({
     title: body.title,    
