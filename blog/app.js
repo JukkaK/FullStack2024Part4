6859a0eddfb029498e76ed60287f28b79767ced3
JukkaK <jukka.koskelin@zure.com>
2024-02-25 09:34:26 +0200

    4.18

--- blog/app.js
+++ blog/app.js
@@ -3,11 +3,13 @@ const express = require('express')
 const app = express()
 require('express-async-errors')
 const cors = require('cors')
+const loginRouter = require('./controllers/login')
 const blogRouter = require('./controllers/blogcontroller')
 const usersRouter = require('./controllers/users')
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
+
 
 mongoose.set('strictQuery', false)
 
@@ -30,8 +32,10 @@ app.use(cors())
 app.use(express.json())
 //app.use(express.static('dist'))
 app.use(middleware.requestLogger)
+
 app.use('/api/blogs', blogRouter)
-app.use('/api/users', usersRouter);
+app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
